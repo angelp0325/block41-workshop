824f8e6b9b0a93fa2bd595711f6d96deb26c3ef1
Angel Polanco <angelepolanco325@gmail.com>
2025-11-23 08:25:33 -0500

    queries/playlists

--- db/queries/playlists.js
+++ db/queries/playlists.js
@@ -1,34 +1,21 @@
 import db from "#db/client";
 
-export async function createPlaylist(name, description) {
-  const sql = `
-  INSERT INTO playlists
-    (name, description)
-  VALUES
-    ($1, $2)
-  RETURNING *
-  `;
+export async function createPlaylist(name, description, userId) {
+  const sql = `INSERT INTO playlists (name, description, user_id) VALUES ($1, $2, $3) RETURNING *`;
   const {
     rows: [playlist],
-  } = await db.query(sql, [name, description]);
+  } = await db.query(sql, [name, description, userId]);
   return playlist;
 }
 
-export async function getPlaylists() {
-  const sql = `
-  SELECT *
-  FROM playlists
-  `;
-  const { rows: playlists } = await db.query(sql);
-  return playlists;
+export async function getPlaylistsByUserId(userId) {
+  const sql = `SELECT * FROM playlists WHERE user_id = $1`;
+  const { rows } = await db.query(sql, [userId]);
+  return rows;
 }
 
 export async function getPlaylistById(id) {
-  const sql = `
-  SELECT *
-  FROM playlists
-  WHERE id = $1
-  `;
+  const sql = `SELECT * FROM playlists WHERE id = $1`;
   const {
     rows: [playlist],
   } = await db.query(sql, [id]);
